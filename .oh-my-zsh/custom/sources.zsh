9d37c10ea5ec25df6253f6cb00c1ef353a5cf938
murchu27 <m.b.murphy15@gmail.com>
2021-09-05 16:26:16 +0100

    Adding some environment and PATH setup to `zsh`

---
+++ .oh-my-zsh/custom/sources.zsh
@@ -0,0 +1,21 @@
+# include PATH to go (if it exists on this system)
+if [ -d "/usr/local/go/bin" ] ; then
+    export PATH=$PATH:/usr/local/go/bin
+fi
+
+# setup environment for cargo (if it exists on this system)
+if [ -d "$HOME/.cargo/env" ] ; then
+    . "$HOME/.cargo/env"
+fi
+
+# setup environment for nvm (if it is used on this system)
+if [ -d "$HOME/.nvm" ] ; then
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+
+# setup environment for login shell
+if [ -f "$HOME/.env" ]; then
+    . "$HOME/.env"
+fi
